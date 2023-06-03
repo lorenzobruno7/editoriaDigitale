a044b0e5264b62854a744b74abebaea57ca1191d
Lorenzo <134946910+lorenzobruno7@users.noreply.github.com>
2023-06-03 18:18:38 +0200

    Update fromHTML2EPUB.bat

--- fromHTML2EPUB.bat
+++ fromHTML2EPUB.bat
@@ -1,1 +1,1 @@
-pandoc project.html --css=epub.css --epub-metadata=metadata.xml -o prova.epub
+pandoc project.html --css=epub2.css --epub-metadata=metadata.xml --epub-embed-font=..\fonts\Josefin_Sans\static\JosefinSans_ExtraLight.ttf --toc --standalone --embed-resources -o prova.epub 
